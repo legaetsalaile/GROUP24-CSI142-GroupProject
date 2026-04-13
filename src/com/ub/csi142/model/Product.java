3ea8a39ab58aad1e8c6a01a93eab54d613de3ff4
unknown <202506747>
2026-04-13 20:57:35 +0200

    Removed code that i added by myself instead of the group

--- src/com/ub/csi142/model/Product.java
+++ src/com/ub/csi142/model/Product.java
@@ -1,59 +1,6 @@
 package com.ub.csi142.model;
 
 public abstract class Product{
-    private String name;
-    private String productID;
-    private int quantity;
-    private double price;
-
-// Constructor 
-
-public Product(String name, String productID, int quantity, double price){
-    this.name=name;
-    this.productID=productID;
-    this.quantity=quantity;
-    this.price=price; 
-}
-
-//Geters 
-
-public String getName(){
-return name;
-}
-
-public String getProductID(){
-    return productID;
-}
-
-public int getQuantity(){
-    return quantity; 
-}
-
-public int getPrice(){
-    return price;
-}
-
-//Setters
-
-public void setName(String name){
-    this.name=name;
-}
-
-public void setProductID(String productID){
-    this.productID=productID;
-}
-
-public void setQuantity(int quantity){
-    this.quantity=quantity;
-}
-
-public void setPrice(double price){
-    this.price=price;
-}
-
-public double TotalValue(){
-   return price * quantity;
-}
-public abstract void display();
+   
 }
 
