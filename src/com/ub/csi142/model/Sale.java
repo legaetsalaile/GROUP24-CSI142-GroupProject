4828294f891de76004549b5b75bcfd9284648024
sootetsilaone168 <sootetsilaone168@gmail.com>
2026-04-14 17:04:09 +0200

    Added Sales.java

--- src/com/ub/csi142/model/Sale.java
+++ src/com/ub/csi142/model/Sale.java
@@ -1,5 +1,32 @@
 package com.ub.csi142.model;
 
-public class Sale{
+public class Sale{ 
+    private String productName;
+    private int quantity;
+    private double price;
+
     
+    public Sales(String productName, int quantity, double price) {
+        this.productName = productName;
+        this.quantity = quantity;
+        this.price = price;
+    }
+    public double calculateTotalSales() {
+        return quantity * price;
+    }
+
+
+    public void displaySales() {
+        System.out.println("Product: " + productName);
+        System.out.println("Quantity : " + quantity);
+        System.out.println("Price t: " + price);
+        System.out.println("Total Sales: " + calculateTotalSales());
+    }
+
+
+    public static void main(String[] args) {
+        Sales sale1 = new Sales("Iphone", 5, 7500.00);
+        sale1.displaySales();
+    }
 }
+    
