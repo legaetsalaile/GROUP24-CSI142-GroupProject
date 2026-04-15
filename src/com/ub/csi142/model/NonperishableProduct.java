a559ed4845f03a40148d69cd8b7f2fb9d7bed450
sootetsilaone168 <sootetsilaone168@gmail.com>
2026-04-16 01:08:34 +0200

    NonperishableProduct.java

--- src/com/ub/csi142/model/NonperishableProduct.java
+++ src/com/ub/csi142/model/NonperishableProduct.java
@@ -1,5 +1,19 @@
 package com.ub.csi142.model;
 
 public class NonperishableProduct{
+    public NonPerishableProduct(String name, String productID, Int quantity,Double price,){
+        super(name,productID,quantity,price,expiryDate);
+    }
+
+    @Override
+    public boolean isperishable(){    
+    return false;
+}
+
+    @Override
+    System.out.println("Non-perishable Product:"+ getName());
+    System.out.println("ProductID:"+ getProductID());
+    System.out.println("Quantity:"+ getquantity());
+    System.out.println("Price:"getprice());
     
 }
