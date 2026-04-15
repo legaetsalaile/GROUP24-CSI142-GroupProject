87bcc91d22b4ed905d1c80f988738e088b83e9d7
sootetsilaone168 <sootetsilaone168@gmail.com>
2026-04-15 16:04:44 +0200

    SalesManager

--- src/com/ub/csi142/service/SalesManager.java
+++ src/com/ub/csi142/service/SalesManager.java
@@ -2,4 +2,12 @@ package com.ub.csi142.service;
 
 public class SalesManager{
     
+    public void recordSale(Inventory inventory){
+System.out.println("Record sale");
+    }
+
+    public void viewSalesReport(){
+        System.out.println("View sales Report");
+    }
 }
+    
