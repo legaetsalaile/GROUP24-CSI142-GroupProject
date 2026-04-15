225861d9a5a295f4dfb967b02fc84484237ac0f2
Mokgatlhepako <202402590@ub.ac.bw>
2026-04-15 16:45:16 +0200

    InputHelper.java

--- src/com/ub/csi142/util/InputHelper.java
+++ src/com/ub/csi142/util/InputHelper.java
@@ -1,5 +1,16 @@
 package com.ub.csi142.util;
 
 public class InputHelper{
+    private Scanner scanner= new Scanner(System.in);
+
+public int getInt(String prompt){
+    System.out.println(prompt);
+    return scanner.nextInt();
+}
     
 }
+
+
+
+
+
