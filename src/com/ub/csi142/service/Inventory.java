22b06de23e9976bd7f02cc44ac3538efc881a19b
sootetsilaone168 <sootetsilaone168@gmail.com>
2026-04-15 16:02:22 +0200

    Inventory.java

--- src/com/ub/csi142/service/Inventory.java
+++ src/com/ub/csi142/service/Inventory.java
@@ -1,5 +1,14 @@
 package com.ub.csi142.service;
 
 public class Inventory{
-    
+    public void addProduct(){
+        System.out.println("Product Name:");
+    }
+    public void viewStock(){
+        System.out.println("view Stock");
+    } 
+    public void searchProduct(){
+        System.out.println("Search product");
+    }
 }
+    
