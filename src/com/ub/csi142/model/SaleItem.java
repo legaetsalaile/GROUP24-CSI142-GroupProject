95cb022bfe082b78ebdfc1be4c646580044d66ac
sootetsilaone168 <sootetsilaone168@gmail.com>
2026-04-21 19:20:41 +0200

    added

--- src/com/ub/csi142/model/SaleItem.java
+++ src/com/ub/csi142/model/SaleItem.java
@@ -1,5 +1,59 @@
 package com.ub.csi142.model;
 
-public class SaleItem{
+public class SaleItem {
+    private String name;
+    private double price;
+    private int quantity;
+
+
+    public SaleItem(String name, double price, int quantity) {
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
     
+    public SaleItem() {
+        this.name = "Unknown";
+        this.price = 0.0;
+        this.quantity = 0;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    // Setters
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    
+    public double getTotalPrice() {
+        return price * quantity;
+    }
+
+    public void displayItem() {
+        System.out.println("Item Name: " + name);
+        System.out.println("Price: " + price);
+        System.out.println("Quantity: " + quantity);
+        System.out.println("Total: " + getTotalPrice());
+    }
 }
