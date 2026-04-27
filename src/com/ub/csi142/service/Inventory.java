6a5737cf4979db27e6b693b37a734e9ef2fc4978
sootetsilaone168 <sootetsilaone168@gmail.com>
2026-04-27 15:05:43 +0200

    Updated Inventory.java

--- src/com/ub/csi142/service/Inventory.java
+++ src/com/ub/csi142/service/Inventory.java
@@ -1,14 +1,61 @@
 package com.ub.csi142.service;
-
 public class Inventory{
-    public void addProduct(){
-        System.out.println("Product Name:");
+private ArrayList<Product> products;
+
+public inventory(){
+    products=new ArrayList<>();
+}
+public void addProduct(Product product){
+    products.add(product);
+    System.out.printIn(product.getname() + "has been added to the inventory.");
+}
+public void view products(){
+    if (products.isEmpty()){
+        System.out.printIn("Inventory is empty.");
+        return;
     }
-    public void viewStock(){
-        System.out.println("view Stock");
-    } 
-    public void searchProduct(){
-        System.out.println("Search product");
+    System.out.printIn("\n--- Inventory List---");
+    for (Product p : products){
+        System.out.printIn(p);
     }
 }
-    
+public Product searchProduct(String name){
+    for (Product p : products){
+        if (p.getName().equalsIgnoreCase(name)){
+            return p;
+        }
+    }
+    return null;
+}
+public void updateStock(String name,int quantity){
+    Product product = searchProduct(name);
+
+    if (Product != null){
+        product.setQuantity(product.getQuantity() + quantity);
+        System.out. printIn("Stock  updated successfully.");
+    } else {
+        System.out.printIn("Product not found.");
+    }
+}   
+ public void sellProduct(String name,int quantity){
+    Product product = searchProduct(name);
+
+    if (product != null){
+        if (product.getquantity() >= quantity){
+            product.setQuantity(product.getQuantity() - quantity);
+            System.out.printIn("Sale successfull.");
+        } else {
+            System.out.printIn("Not enough stock.");
+        } else {
+            System.out.printIn("Product not found.");
+        }
+    }
+}
+
+ }
+
+
+
+
+
+ 
